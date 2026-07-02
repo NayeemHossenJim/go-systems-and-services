6a953cdc71664af82e21e6cb15c5be441c754b9e
NayeemHossenJim <nayeemhossenjim@gmail.com>
2026-07-02 12:38:57 +0600

    Basic Types

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
+
 import "fmt"
 
+type numVar int
+type strVar string
+type boolVar bool
+
 func main() {
-	fmt.Println("Hello World")
+	var intVar numVar = 98
+	var phraseVar strVar = "checking string"
+	var falseVar boolVar = false
+	fmt.Printf("%v\n", intVar)
+	fmt.Printf("%T\n", intVar)
+	fmt.Printf("%v\n", phraseVar)
+	fmt.Printf("%T\n", phraseVar)
+	fmt.Printf("%v\n", falseVar)
+	fmt.Printf("%T\n", falseVar)
 }
